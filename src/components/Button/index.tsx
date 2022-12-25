34e1e539e87e9d1bef6673e8d6f6695cb39c3dd4
ledrol <88734133+ledrol@users.noreply.github.com>
2022-12-25 11:39:01 -0300

    Resolução do desafio

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,8 @@
 import { ButtonContainer } from "./styles";
 import { IButtonProps } from "./types";
 
-const Button = ({ title, onClick }: IButtonProps) => {
-  return <ButtonContainer onClick={onClick}>{title}</ButtonContainer>;
+const Button = ({ title, onClick, enabled}: IButtonProps) => {
+  return <ButtonContainer onClick={onClick} disabled={!enabled} >{title}</ButtonContainer>;
 };
 
 export default Button;
--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -1,4 +1,5 @@
 export interface IButtonProps {
     title: string;
+    enabled: boolean;
     onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
